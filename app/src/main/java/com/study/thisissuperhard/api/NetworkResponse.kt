7e9c4964b9824811acfa173f71e1a61747dd6fe8
aa_m1_a <ammarayub2112@gmail.com>
2025-03-14 16:54:07 +0800

    first commit from non main branch

--- app/src/main/java/com/study/thisissuperhard/api/NetworkResponse.kt
+++ app/src/main/java/com/study/thisissuperhard/api/NetworkResponse.kt
@@ -2,6 +2,7 @@ package com.study.thisissuperhard.api
 
 // use T instead of WeatherModel so that we can use it everywhere
 sealed class NetworkResponse<out T> {
+//    important commit ammr-march
     data class Success<out T>(val data : T) : NetworkResponse<T>()
     data class Error(val message : String) : NetworkResponse<Nothing>()
     object Loading : NetworkResponse<Nothing>()
